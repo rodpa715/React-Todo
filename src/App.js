d0061eece23bce1c7b90d13100200330bc3a734e
Patrick Rodrigues <patrick5rodrigues@outlook.com>
2019-05-30 15:25:29 +0200

    fix asynchronous state update bug

--- src/App.js
+++ src/App.js
@@ -1,103 +1,116 @@
-import React from 'react';
-import TodoComponent from './components/TodoComponents/Todo'
-import localStorageHelper from "./localStorageHelper"
-import uuid from 'uuid';
+import React from "react";
+import TodoComponent from "./components/TodoComponents/Todo";
+import localStorageHelper from "./localStorageHelper";
+import uuid from "uuid";
 
 const initialTodoList = [
-    {
+  {
     id: uuid(),
-    task: "Click on me and click Clear Completed Tasks", 
+    task: "Click on me and click Clear Completed Tasks",
     completed: false
-    },
-    {
+  },
+  {
     id: uuid(),
-    task: "Clear All will delete all tasks, even completed ones", 
+    task: "Clear All will delete all tasks, even completed ones",
     completed: false
-    },
-  ];
-  
+  }
+];
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
-  
-  constructor(props){
-    super(props)
+
+  constructor(props) {
+    super(props);
     this.state = {
       todoList: localStorageHelper.getInitialObject("state", initialTodoList),
-      newTodoTask: "",
-    }
+      newTodoTask: ""
+    };
   }
-
-  /** Updates input on keypress**/
-  changeHandler = (e) => {
-    this.setState({
-      newTodoTask: e.target.value,
+  updateComponent() {
+    this.setState(state => {
+      localStorageHelper.updateLocalStorage("state", state.todoList);
     });
   }
+  /** Updates input on keypress**/
+  changeHandler = e => {
+    this.setState({
+      newTodoTask: e.target.value
+    });
+  };
 
   /** invokes addTask() on Enter keypress**/
-  enterHandler =  (e) => e.key === "Enter" ? this.addTask() : null
-  
+  enterHandler = e => (e.key === "Enter" ? this.addTask() : null);
+
   /** adds a new object to the array **/
   addTask = () => {
-    const newTodoItem = {
-      id: uuid(),
-      task: this.state.newTodoTask === "" ? "Nothing bro" : this.state.newTodoTask,
-      completed: false,
-    }
-    
-    const newTodoList = this.state.todoList.concat(newTodoItem)
-    
-    this.setState({
-      todoList: newTodoList,
-      newTodoTask: "",
-    })
-    localStorageHelper.updateLocalStorage("state", this.state.todoList)
-  }
+    this.setState(state => {
+      const newTodoItem = {
+        id: uuid(),
+        task: state.newTodoTask === "" ? "Nothing bro" : this.state.newTodoTask,
+        completed: false
+      };
+
+      const newTodoList = state.todoList.concat(newTodoItem);
+
+      return {
+        todoList: newTodoList,
+        newTodoTask: ""
+      };
+    });
+
+    this.updateComponent();
+  };
 
   /** Clears every object to witch it's completion state is true **/
   clearCompleted = () => {
     this.setState(state => ({
       todoList: state.todoList.filter(item => item.completed === false)
     }));
-    localStorageHelper.updateLocalStorage("state", this.state.todoList)
-  }
+
+    this.updateComponent();
+  };
 
   /** Removes every item from todo list **/
   clearAll = () => {
-    this.setState({
-      todoList: []
-    })
-    localStorageHelper.clearLocalStorage("state")
-  }
-  
+    this.setState(state => {
+      return {
+        todoList: []
+      };
+    });
+
+    localStorageHelper.clearLocalStorage("state");
+  };
+
   /** Toggles the completion state of the element being clicked to false and true **/
   toggleCompleted = id => {
-
     const updateState = list => {
-      this.setState({
-        todoList: list,
-      })
-    }
+      this.setState(state => {
+        return {
+          todoList: list
+        };
+      });
+    };
 
-    let newTodoList = this.state.todoList.slice()
+    let newTodoList = this.state.todoList.slice();
 
-    newTodoList = newTodoList.map( item => {
-      if(item.id === id){
+    newTodoList = newTodoList.map(item => {
+      if (item.id === id) {
         item.completed = !item.completed;
-        return updateState(newTodoList)
-      }else{
-        return updateState(newTodoList)
+        return updateState(newTodoList);
+      } else {
+        return updateState(newTodoList);
       }
-    })
-    localStorageHelper.updateLocalStorage("state", this.state.todoList)
-  }
+    });
+
+    this.updateComponent();
+  };
 
   /* renders the component */
   render() {
     return (
-      <TodoComponent 
+      <TodoComponent
         newToDo={this.state.newTodoTask}
         toDoList={this.state.todoList}
         addTask={this.addTask}
